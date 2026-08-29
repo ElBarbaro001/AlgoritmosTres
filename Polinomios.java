d833c6198e7c40f22ef69b5da5ab488c82b9eef1
ElBarbaro001 <casystem0@gmail.com>
2026-08-29 12:15:20 -0500

    Polinomios.java

--- Polinomios.java
+++ Polinomios.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
 public class Polinomios{
-    
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese la dimension del vector:");
+        int tam = teclado.nextInt();
+        int valor = 0;
+        int[] arrpolinomio = new int[tam];
+        for(int i=0; i<tam; i++){
+            System.out.println("Ingrese para la posicion [" + (i) + "]:");
+            valor = teclado.nextInt();
+            arrpolinomio[i] = valor;
+        }
+        System.out.println("Imprimir Arreglo:");
+        for(int i=0; i<tam; i++){
+            System.out.print(arrpolinomio[i] + " ");
+        }
+    }
 }
